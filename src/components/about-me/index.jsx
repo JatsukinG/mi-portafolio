ba4e0de76f895a1eb135b056aa4e996ca2c875d2
Julian-Trujillo <julian@msbloyalty.com>
2023-01-04 12:34:30 -0500

    Added responsive classes

--- src/components/about-me/index.jsx
+++ src/components/about-me/index.jsx
@@ -1,9 +1,9 @@
 
 const AboutMe = () => {
   return (
-      <div className="w-full h-full bg-gray-100 flex justify-around items-center">
+      <div className="w-full h-full bg-gray-100 flex md:flex-row flex-col justify-around items-center">
         <h2 className="text-4xl font-bold">About me</h2>
-        <p className="font-semibold">
+        <p className="font-semibold text-center p-8">
           I studied Mechatronic Engineering, but I like so much the web programming
         </p>
       </div>
